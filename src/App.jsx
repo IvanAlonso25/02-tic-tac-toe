86605b691763ae1bbc6a771df9f481642dbfadf5
IvanAlonso25 <ialonso479@gmail.com>
2024-08-25 23:28:11 +0200

    Extract code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import confetti from 'canvas-confetti'
 import './App.css'
-import {Square} from './components/Square.jsx'
-import {TURNS} from './constants.js'
-import {checkWinnerFrom, checkEndGame} from './logic/board.js'
-import {WinnerModal} from './components/WinnerModal.jsx'
-import {Board} from './components/Board.jsx'
+import { Square } from './components/Square.jsx'
+import { TURNS } from './constants.js'
+import { checkWinnerFrom, checkEndGame } from './logic/board.js'
+import { WinnerModal } from './components/WinnerModal.jsx'
+import { Board } from './components/Board.jsx'
 
 function App() {
-  const [board, setBoard] = useState(
-    Array(9).fill(null)
-  )
 
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if (boardFromStorage) return JSON.parse(boardFromStorage)
+    return Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    if (turnFromStorage) return turnFromStorage
+    return TURNS.X
+  })
   const [winner, setWinner] = useState(null)
 
   const resetGame = () => {
@@ -20,11 +27,14 @@ function App() {
     setBoard(Array(9).fill(null))
     // setTurn(TURNS.X)
     setWinner(null)
+    // Eliminamos al resetear el juego el localStorage
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   }
 
   const updateBoard = (index) => {
     // No actualizamos la posición si ya tiene algo
-    if(board[index] || winner) return
+    if (board[index] || winner) return
     // Los ... hacen una copia del array para no modificar los datos
     const newBoard = [...board]
     newBoard[index] = turn
@@ -32,6 +42,9 @@ function App() {
     // Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+    // guardar estado del tablero
+    // window.localStorage.setItem('board', JSON.stringify(newBoard))
+    // window.localStorage.setItem('turn', turn)
     // revisamos si hay un ganador cada vez que actualizamos el board
     const newWinner = checkWinnerFrom(newBoard)
     if (newWinner) {
@@ -51,7 +64,7 @@ function App() {
     <main className='board'>
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset del juego</button>
-      <Board board={board} updateBoard={updateBoard}/>
+      <Board board={board} updateBoard={updateBoard} />
 
       <section className="turn">
         <Square isSelected={turn === TURNS.X}>
@@ -62,7 +75,7 @@ function App() {
         </Square>
       </section>
 
-      <WinnerModal resetGame={resetGame} winner={winner}/>
+      <WinnerModal resetGame={resetGame} winner={winner} />
     </main>
   )
 }
